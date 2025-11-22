816fdbbae0d7e1f17550fb6cb81c8a273715b9b6
Momo <75737534+MTsocute@users.noreply.github.com>
2025-11-22 23:16:38 +0800

    Add Config For My Mac
    新增 Mac 端的 Vscode 配置

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include "main.h"
 
-class Solution
+
+template <class T>
+concept Addable = requires(T x, T y) { x + y; };
+
+template <Addable T>
+auto add(T a, T b) -> decltype(a + b)
 {
-};
+    return a + b;
+}
 
 int main()
 {
+    fmt::print("Ans = {}\n", add(3, 5));
     return 0;
 }
